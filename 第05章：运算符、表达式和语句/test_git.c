da382ae646c93189db551f1b3b5e96e18e40e705
李炜宸 <1511309453@qq.com>
2026-09-17 13:18:59 +0800

    Update test_git.c

--- 第05章：运算符、表达式和语句/test_git.c
+++ 第05章：运算符、表达式和语句/test_git.c
@@ -1,1 +1,11 @@
-// Hello! This,is a test to add new content to Github!
+// 版本2：会算加法的程序（从一行注释升级成完整程序）
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b;
+    printf("请输入两个整数：");
+    scanf("%d %d", &a, &b);
+    printf("%d + %d = %d\n", a, b, a + b);
+    return 0;
+}
