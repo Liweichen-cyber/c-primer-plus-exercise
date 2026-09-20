62d386f81484b3799440589dda8914ef22123fe1
李炜宸 <299988803+Liweichen-cyber@users.noreply.github.com>
2026-09-20 21:10:29 +0800

    Complete Ch6 Ex12(First Series):harmonic first series calculation 第一阶段，每项均为正数

---
+++ 第06章：C控制语句：循环/test12.c
@@ -0,0 +1,22 @@
+#include  <stdio.h>
+int main (void) {
+
+    int number , i ;
+    double sum  ;
+    printf("Please input a number to sum up :");
+    scanf ("%d" , &number);
+
+    while (number > 0 ) {
+        sum = 0 ;     // 初始化累加变量：必须在 for 循环开始前，否则每轮都会清零，无法累加
+        for (i = 1 ; i <= number ; i ++) {
+            sum = sum + 1.0 /i ;     // 1.0 使除法变为浮点除法；1 / i 是整数除法，小数部分会被舍弃
+        }
+        printf("%f\n" , sum);
+        printf("Please input another number to sum up:");
+        scanf ("%d" , &number) ;
+        }
+
+    return 0;
+    }
+
+
