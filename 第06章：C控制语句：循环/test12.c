7d39300a2e81598b2d331a9098390723a77d418b
李炜宸 <299988803+Liweichen-cyber@users.noreply.github.com>
2026-09-21 11:12:47 +0800

    Complete Ch6 Ex12(Second Series):harmonic first series calculation 用if语句来控制某个项为加还是减，if for while共同使用

--- 第06章：C控制语句：循环/test12.c
+++ 第06章：C控制语句：循环/test12.c
@@ -2,21 +2,33 @@
 int main (void) {
 
     int number , i ;
-    double sum  ;  //不能用int sum
+    double sum1 , sum2  ;  //不能用int sum
     printf("Please input a number to sum up :");
     scanf ("%d" , &number);
 
     while (number > 0 ) {
-        sum = 0 ;     // 初始化累加变量：必须在 for 循环开始前，否则每轮都会清零，无法累加
+        sum1 = 0 , sum2 = 0 ;   // 初始化累加变量：必须在 for 循环开始前，否则每轮都会清零，无法累加
         for (i = 1 ; i <= number ; i ++) {
-            sum = sum + 1.0 /i ;     // 1.0 使除法变为浮点除法；1 / i 是整数除法，小数部分会被舍弃
+            sum1 = sum1 + 1.0 /i ;       // 1.0 使除法变为浮点除法；1 / i 是整数除法，小数部分会被舍弃
+
+            if (i % 2 == 0) {            //通过if语句判断奇偶，注意不需要修改for循环的条件
+                sum2 = sum2 - 1.0/i ;
+            }
+            else {
+                sum2 = sum2 + 1.0/i ;
+            }
         }
-        printf("Sequence 1 :%f\n" , sum);
+        printf("Sequence 1 :%f\n" ,  sum1);
+        printf("Sequence 2 :%f\n" ,  sum2);
         printf("Please input another number to sum up:");
-        scanf ("%d" , &number) ;
+        scanf ("%d" , &number) ;            //需要用户再次输入，用到scanf，否则将一直循环下去，变成无限循环
         }
+
 
     return 0;
     }
+
+
+
 
 
