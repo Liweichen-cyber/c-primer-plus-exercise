a73e02aed27c88616c577d0a96b5d5a1c47e689d
李炜宸 <299988803+Liweichen-cyber@users.noreply.github.com>
2026-09-24 14:21:25 +0800

    Complete Ch7 Ex1：Count the number of characters

---
+++ 第07章：C控制语句：分支和跳转/test1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void) {
+
+    //char ch ;   whiile 里面有ch = getchar了，这里就不用再写了
+    printf ("Please enter a paragraph of text :") ;
+    ch = getchar() ;
+    int space = 0 ;
+    int newlines = 0 ;
+    int others = 0 ;
+
+    while ((ch = getchar()) != '#') {  //ch = getchar 要打括号，注意运算的优先级，同时#作为特殊字符需要打单引号
+        if (ch == ' ') {
+            space ++ ;
+        }
+        else if (ch == '\n') {
+            newlines ++ ;
+        }
+        else {
+            others ++ ;
+        }
+
+    }
+    printf ("The number of the spaces , newlines , other charactars are %d , %d ,and %d \n" , space , newlines , others);
+    return 0;
+
+}
